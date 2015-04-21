d520dfe945bbbc5467d38cd688dd529f347a3595
Antiserum82 <patrick.schoonheym@student.hu.nl>
2015-04-21 12:58:50 +0200

    opdracht 3

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -1,5 +1,6 @@
 #include "StudentPreProcessing.h"
 #include "IntensityImageStudent.h"
+#include "ImageIO.h"
 
 IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &image) const {
 	IntensityImageStudent* IntensityImage = new IntensityImageStudent{ image.getWidth(), image.getHeight() };
@@ -19,7 +20,26 @@ IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &imag
 }
 
 IntensityImage * StudentPreProcessing::stepEdgeDetection(const IntensityImage &image) const {
-	return nullptr;
+
+	IntensityImageStudent* IntensityImage = new IntensityImageStudent{ image.getWidth(), image.getHeight() };
+
+	const int Gaussian_mask[] = { 0, 1, 0,
+								  1, -4, 1,
+								  0, 1, 0 };
+
+	const int OFFSET = 10;
+	
+	for (int i = image.getWidth() + 1; i < (image.getHeight() * image.getWidth()) - ((image.getWidth() +1)+ image.getWidth()); i++) {
+
+		if (i % image.getWidth() == 0 || i % image.getWidth() == image.getWidth()-1){
+			continue;
+		}
+		IntensityImage->setPixel(i, Intensity{ 255 });	
+	}
+
+	ImageIO::saveIntensityImage(image, ImageIO::getDebugFileName("C:\\Users\\Patrick\\Documents\\GitHub\\HU-Vision-1415-Base\\debug\\test.png"));
+
+	return IntensityImage;
 }
 
 IntensityImage * StudentPreProcessing::stepThresholding(const IntensityImage &image) const {
--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.h
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.h
@@ -7,6 +7,7 @@
 #pragma once
 #include "PreProcessing.h"
 class StudentPreProcessing : public PreProcessing {
+
 public:
 	IntensityImage * stepToIntensityImage(const RGBImage &image) const;
 	IntensityImage * stepScaleImage(const IntensityImage &image) const;
